58225b26683565acb3d0c6e4675e5aa97e986f54
吳冠霖 <ckjeter7@gmail.com>
2021-01-03 23:28:43 +0800

    sram fix

--- dcache_sram.v
+++ dcache_sram.v
@@ -29,7 +29,7 @@ output             hit_o;
 // Memory
 reg      [24:0]    tag [0:15][0:1];    
 reg      [255:0]   data[0:15][0:1];
-reg                use_rec [0:15][0:1];
+reg                use_rec [0:15];
 integer            i, j;
 
 reg   [24:0]    tag_o;
@@ -42,26 +42,24 @@ reg             hit_o;
 always@(posedge clk_i or posedge rst_i) begin
     if (rst_i) begin
         for (i=0;i<16;i=i+1) begin
+            use_rec[i] <= 1'b0;
             for (j=0;j<2;j=j+1) begin
                 tag[i][j] <= 25'b0;
                 data[i][j] <= 256'b0;
-                use_rec[i][j] <= 1'b0;
             end
         end
     end
     if (enable_i && write_i) begin
         // TODO: Handle your write of 2-way associative cache + LRU here
-        if (use_rec[addr_i][0]==1'b0) begin
+        if (use_rec[addr_i]==1'b0) begin
             tag[addr_i][0]<=tag_i;
             data[addr_i][0]<=data_i;
-            use_rec[addr_i][0]<=1'b1;
-            use_rec[addr_i][1]<=1'b0;
+            //use_rec[addr_i]<=1'b1;
         end
         else begin //use_rec[addr_i][1]==1'b0
             tag[addr_i][1]<=tag_i;
             data[addr_i][1]<=data_i;
-            use_rec[addr_i][1]<=1'b1;
-            use_rec[addr_i][0]<=1'b0;
+            //use_rec[addr_i]<=1'b0;
         end
 
     end
@@ -75,11 +73,13 @@ always@(*) begin
             hit_o=1'b1;
             data_o=data[addr_i][0];
             tag_o=tag[addr_i][0];
+            use_rec[addr_i]<=1'b0;
         end
         else if ((tag_i[22:0] == tag[addr_i][1][22:0]) && tag[addr_i][1][24]) begin
             hit_o=1'b1;
             data_o=data[addr_i][1];
             tag_o=tag[addr_i][1];
+            use_rec[addr_i]<=1'b1;
         end
         else begin
             hit_o=1'b0;
